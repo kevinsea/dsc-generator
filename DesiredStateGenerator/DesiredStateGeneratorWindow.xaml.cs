8144601230cbb67000c257f4f1e5fcbde6084a55
Peter Schuster <development@peschuster.de>
2016-04-16 13:53:51 +0200

    Log exceptions
    Log exceptions to trace log (including stack trace for debugging).

--- DesiredStateGenerator/DesiredStateGeneratorWindow.xaml.cs
+++ DesiredStateGenerator/DesiredStateGeneratorWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -44,6 +45,7 @@ namespace DesiredState
 			}
 			catch (Exception ex)
 			{
+				Trace.TraceError(ex.ToString());
 				MessageBox.Show(ex.Message);
 			}
 			finally
